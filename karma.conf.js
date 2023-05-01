f884ab97a0cf52bde0dd7111cf1cb8de64677a63
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-05-01 15:25:24 +0700

    membuat testing otomatis like restaurant specs Kutangkap Bugs Dengan Jaring Pengaman

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration
-// Generated on Sun Apr 30 2023 20:35:40 GMT+0700 (Western Indonesia Time)
-
+// Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
 module.exports = function (config) {
   config.set({
 
@@ -8,26 +7,41 @@ module.exports = function (config) {
     basePath: '',
 
     // frameworks to use
-    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.js',
+      'specs/**/*Spec.js',
     ],
 
     // list of files / patterns to exclude
-    exclude: [
-    ],
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
-    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'specs/**/*Spec.js': ['webpack', 'sourcemap'],
+    },
+
+    webpack: {
+      // karma watches the test entry points
+      // (you don't need to specify the entry option)
+      // webpack watches dependencies
+      // webpack configuration
+      devtool: 'inline-source-map',
+      mode: 'development',
+    },
+
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      // i. e.
+      stats: 'errors-only',
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
-    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
     // web server port
@@ -37,15 +51,15 @@ module.exports = function (config) {
     colors: true,
 
     // level of logging
-    // eslint-disable-next-line max-len
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    /* possible values: config.LOG_DISABLE || config.LOG_ERROR
+    || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG */
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
     // start these browsers
-    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
     // Continuous Integration mode
@@ -53,7 +67,7 @@ module.exports = function (config) {
     singleRun: false,
 
     // Concurrency level
-    // how many browser instances should be started simultaneously
+    // how many browser should be started simultaneous
     concurrency: Infinity,
   });
 };
---
+++ specs/likeRestaurantSpec.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-undef */
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestaurantIdb from '../src/scripts/data/favorite-restaurant-idb';
+
+describe('Menyukai sebuah restorant', () => {
+  const addLikeButtonContainer = () => {
+    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
+
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
+  // tes ke 1
+  it('harus memperlihatkan sebuah tombol like ketika sebuah restoran tidak disukai sebelumnya', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    expect(document.querySelector('[aria-label="like this movie"]'))
+      .toBeTruthy();
+  });
+  // tes ke 2
+  it('tidak boleh menampilkan tombol unlike ketika restoran tidak disukai sebelumnya', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    expect(document.querySelector('[aria-label="unlike this movie"]'))
+      .toBeFalsy();
+  });
+  // tes ke 3
+  it('harus bisa menyediakan untuk menyukai restoran', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    const restaur = await FavoriteRestaurantIdb.getRestaurant(1);
+    expect(restaur).toEqual({ id: 1 });
+
+    FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+  // tes ke 4
+  it('seharusnya tidak menambahkan restorant ketika sudah disukai', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 1,
+      },
+    });
+    // tambahkan restoran dengan id 1 ke daftar resto yang disukai
+    await FavoriteRestaurantIdb.putRestaurant({ id: 1 });
+
+    // simulasikan pengguna menekan tombol suka resto
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    // tidak ada restoran yang ganda
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([{ id: 1 }]);
+
+    FavoriteRestaurantIdb.deleteRestaurant(1);
+  });
+  // tes ke 5
+  xit('harus tidak menambahkan restoran ketika restoran tidak memiliki Id', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {},
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+    expect(await FavoriteRestaurantIdb.getAllRestaurants()).toEqual([]);
+  });
+});
